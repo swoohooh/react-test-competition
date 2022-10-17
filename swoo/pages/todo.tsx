f22b6ca5a5321b95f25c9242a88545f601b4eb30
swoohooh <swoohooh@gmail.com>
2022-10-17 18:20:25 +0000

    Add input and button to todo page

---
+++ swoo/pages/todo.tsx
@@ -0,0 +1,42 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import react, { useState, useEffect } from "react";
+
+const Todo: NextPage = () => {
+  const [item, setItem] = useState<string>("");
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setItem(e.target.value);
+  };
+
+  const handleAdd = () => {
+    setTodos([item, ...todos]);
+    setItem("");
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div>
+      <Head>
+        <title>ToDo</title>
+        <meta name="description" content="ToDo" />
+      </Head>
+
+      <main>
+        <h1>Todo list!</h1>
+        <form>
+          <input type="text" value={item} onChange={handleInput} />
+          <button type="button" onClick={handleAdd}>
+            Add
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default Todo;

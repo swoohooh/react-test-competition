0f42c2eec13004e3d56914b5e9acb20ca895f444
sso214 <sojam214@gmail.com>
2022-10-17 17:52:45 +0900

    feat : TodoList 컴포넌트 조립

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,77 @@
+import { useState } from 'react';
 import type { NextPage } from 'next';
+import styled from 'styled-components';
+import TodoHeader from '../components/TodoHeader';
+import TodoItem from '../components/TodoItem';
+import TodoAddForm from '../components/TodoAddForm';
+
+const TodoContainer = styled.div`
+  max-width: 500px;
+  margin: 50px auto;
+
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+
+  & > div,
+  & > ul {
+    padding-left: 25px;
+    padding-right: 25px;
+  }
+
+  & > ul {
+    padding-bottom: 25px;
+  }
+`;
+
+export type ITodo = { idx: string; task: string; completed: boolean };
 
 const Home: NextPage = () => {
-  return <div></div>;
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const todoAdd = (todo: ITodo) => setTodos([...todos, todo]);
+
+  const todoRemove = (idx: string) => setTodos(todos.filter((item) => item.idx !== idx));
+
+  const todoToggle = (idx: string) => {
+    const updateTodo = todos.map((todo) => {
+      if (todo.idx === idx) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setTodos(updateTodo);
+  };
+
+  const todoUpdate = (idx: string, task: string) => {
+    const updateTodo = todos.map((todo) => {
+      if (todo.idx === idx) {
+        return { ...todo, task: task };
+      }
+      return todo;
+    });
+    setTodos(updateTodo);
+  };
+
+  return (
+    <TodoContainer>
+      <TodoHeader todoLength={todos.length} completedTodoLength={todos.filter((item) => item.completed).length} />
+
+      <TodoAddForm todoAdd={todoAdd} />
+
+      <ul>
+        {todos?.map((todo) => (
+          <TodoItem
+            key={todo.idx}
+            todo={todo}
+            todoToggle={todoToggle}
+            todoUpdate={todoUpdate}
+            todoRemove={todoRemove}
+          />
+        ))}
+      </ul>
+    </TodoContainer>
+  );
 };
 
 export default Home;
